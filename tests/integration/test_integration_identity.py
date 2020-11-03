5500aa19d15bb6dcc36fbbaad62f57a9a7610617
Ash Wilson <ash.wilson@valimail.com>
2020-11-03 14:18:59 -0800

    new: usr: Add Identity.verify_certificate_signature().

--- tests/integration/test_integration_identity.py
+++ tests/integration/test_integration_identity.py
@@ -3,11 +3,13 @@ import os
 
 from dane_discovery.dane import DANE
 from dane_discovery.identity import Identity
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
 
 
 here_dir = os.path.dirname(os.path.abspath(__file__))
 dyn_assets_dir = os.path.join(here_dir, "../fixtures/dynamic/")
 identity_name = "abc123.air-quality-sensor._device.example.net"
+ca_certificate_name = "ca.example.net.cert.pem"
 
 
 class TestIntegrationIdentity:
@@ -36,3 +38,21 @@ class TestIntegrationIdentity:
         print(report)
         assert isinstance(report, str)
         assert "extension" in report
+
+    def test_integration_identity_load_private_key(self):
+        """Test loading a private key from PEM data."""
+        key_pem = self.get_dyn_asset("{}.key.pem".format(identity_name))
+        key_obj = Identity.load_private_key(key_pem)
+        assert isinstance(key_obj, RSAPrivateKey)
+
+    def test_integration_identity_verify_certificate_signature_success(self):
+        """Test CA signature validation success."""
+        entity_certificate = self.get_dyn_asset("{}.cert.pem".format(identity_name))
+        ca_certificate = self.get_dyn_asset(ca_certificate_name)
+        assert Identity.verify_certificate_signature(entity_certificate, ca_certificate)
+
+    def test_integration_identity_verify_certificate_signature_fail(self):
+        """Test CA signature validation failure."""
+        entity_certificate = self.get_dyn_asset("{}.cert.pem".format(identity_name))
+        ca_certificate = self.get_dyn_asset("{}.cert.pem".format(identity_name))
+        assert not Identity.verify_certificate_signature(entity_certificate, ca_certificate)
