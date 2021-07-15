cbc6ea68a2a0cee531e916cef82766fdc15404f7
Ash Wilson <ash.wilson@valimail.com>
2021-07-15 12:29:05 -0700

    chg: test: Create multi-tier PKI for testing. !wip

--- tests/scripts/generate_crypto.sh
+++ tests/scripts/generate_crypto.sh
@@ -14,7 +14,8 @@ LOCAL_DOMAIN="example.net"
 # Base paths
 CRYPTO_EXPORT_PATH="${HOME}/export"
 CRYPTO_DIR="${HOME}/crypto"
-LOCAL_CA_DIR="${CRYPTO_DIR}/local_ca"
+ROOT_CA_DIR="${CRYPTO_DIR}/root_ca"
+INTERMEDIATE_CA_DIR="${CRYPTO_DIR}/intermediate_ca"
 DEV_DIR="${CRYPTO_DIR}/device"
 
 # General device info
@@ -28,9 +29,15 @@ ECC_DIDN_ID="${ECC_DEVICE_SERIAL}.${DEVICE_MODEL}._device.${LOCAL_DOMAIN}"
 
 # Specific file paths
 
-## Local CA
-LOCAL_CA_KEY="${LOCAL_CA_DIR}/ca.example.net.key.pem"
-LOCAL_CA_CERT="${LOCAL_CA_DIR}/ca.example.net.cert.pem"
+## Local Root CA
+ROOT_CA_KEY="${ROOT_CA_DIR}/rootca.example.net.key.pem"
+ROOT_CA_CERT="${ROOT_CA_DIR}/rootca.example.net.cert.pem"
+ROOT_CA_CSR="${ROOT_CA_DIR}/rootca.example.net.csr.pem"
+
+## Local intermediate CA
+INTERMEDIATE_CA_KEY="${INTERMEDIATE_CA_DIR}/intermediateca.example.net.key.pem"
+INTERMEDIATE_CA_CERT="${INTERMEDIATE_CA_DIR}/intermediateca.example.net.cert.pem"
+INTERMEDIATE_CA_CSR="${INTERMEDIATE_CA_DIR}/intermediateca.example.net.csr.pem"
 
 ## RSA Dev
 RSA_DEV_KEY="${DEV_DIR}/${RSA_DIDN_ID}.key.pem"
@@ -48,8 +55,11 @@ ECC_DEV_CERT_DER="${DEV_DIR}/${ECC_DIDN_ID}.cert.der"
 RSA_SSL_CONFIG="${DEV_DIR}/${RSA_DIDN_ID}.conf"
 ECC_SSL_CONFIG="${DEV_DIR}/${ECC_DIDN_ID}.conf"
 
-mkdir -p ${LOCAL_CA_DIR}/demoCA/
-echo "05" > ${LOCAL_CA_DIR}/demoCA/serial 
+mkdir -p ${ROOT_CA_DIR}/demoCA/
+echo "05" > ${ROOT_CA_DIR}/demoCA/serial 
+
+mkdir -p ${INTERMEDIATE_CA_DIR}/demoCA/
+echo "05" > ${INTERMEDIATE_CA_DIR}/demoCA/serial 
 
 ##############
 # Install OpenSSL
@@ -62,43 +72,92 @@ apt-get update && \
 sudo cp ssl.cnf /usr/lib/ssl/openssl.cnf
 cat /usr/lib/ssl/openssl.cnf
 
-
-
 ##############
 # Create dirs
 ##############
 mkdir -p \
     ${CRYPTO_EXPORT_PATH} \
     ${CRYPTO_DIR} \
-    ${LOCAL_CA_DIR} \
     ${DEV_DIR} \
-    ${LOCAL_CA_DIR}/demoCA/
+    ${ROOT_CA_DIR}/demoCA/ \
+    ${ROOT_CA_DIR}/demoCA/newcerts/ \
+    ${INTERMEDIATE_CA_DIR}/demoCA/ \
+    ${INTERMEDIATE_CA_DIR}/demoCA/newcerts/
 
 ##############
-# Create local
-# CA
+# Create self-signed
+# root CA
 ##############
-cd ${LOCAL_CA_DIR}
-openssl genrsa \
-    -out ${LOCAL_CA_KEY} \
-    4096
+echo "Create root CA certificate"
+touch ${ROOT_CA_DIR}/demoCA/index.txt
+touch ${ROOT_CA_DIR}/demoCA/index.txt.attr
+cd ${ROOT_CA_DIR}
 openssl req \
-    -key ${LOCAL_CA_KEY} \
     -new \
-    -x509 \
+    -keyout ${ROOT_CA_KEY} \
+    -nodes \
     -days 7300 \
     -sha256 \
-    -subj "/C=US/ST=CA/O=Example Networks/CN=Example Networks CA" \
-    -out ${LOCAL_CA_CERT}
-openssl x509 -noout -text -in ${LOCAL_CA_CERT}
-touch ${LOCAL_CA_DIR}/demoCA/index.txt
-touch ${LOCAL_CA_DIR}/demoCA/index.txt.attr
+    -subj "/C=US/ST=CA/O=Example Networks/CN=Example Networks Root CA" \
+    -reqexts v3_ca_req \
+    -out ${ROOT_CA_CSR}
+
+cat ${ROOT_CA_CSR}
+
+openssl ca \
+    -selfsign \
+    -keyfile ${ROOT_CA_KEY} \
+    -in ${ROOT_CA_CSR} \
+    -out ${ROOT_CA_CERT} \
+    -name CA_default \
+    -extensions v3_ca \
+    -batch \
+    -verbose 
+
+openssl x509 -noout -text -in ${ROOT_CA_CERT}
+openssl x509 -noout -text -in ${ROOT_CA_CERT} | grep "CA:FALSE" && echo "NOT A CA CERTIFICATE" && exit 1
+
+##############
+# Create 
+# intermediate CA
+##############
+echo "Create intermediate CA certificate"
+touch ${INTERMEDIATE_CA_DIR}/demoCA/index.txt
+touch ${INTERMEDIATE_CA_DIR}/demoCA/index.txt.attr
+cd ${INTERMEDIATE_CA_DIR}
+openssl req \
+    -new \
+    -keyout ${INTERMEDIATE_CA_KEY} \
+    -nodes \
+    -days 7300 \
+    -sha256 \
+    -subj "/C=US/ST=CA/O=Example Networks/CN=Example Networks Intermediate CA" \
+    -reqexts v3_ca_req \
+    -out ${INTERMEDIATE_CA_CSR}
+
+cat ${INTERMEDIATE_CA_CSR}
+
+openssl ca \
+    -keyfile ${ROOT_CA_KEY} \
+    -cert ${ROOT_CA_CERT} \
+    -in ${INTERMEDIATE_CA_CSR} \
+    -out ${INTERMEDIATE_CA_CERT} \
+    -name CA_default \
+    -batch \
+    -extensions v3_ca \
+    -verbose
+
+echo "Check intermediate cert against root cert"
+cat ${INTERMEDIATE_CA_CERT}
+openssl x509 -noout -text -in ${INTERMEDIATE_CA_CERT}
+openssl x509 -noout -text -in ${INTERMEDIATE_CA_CERT} | grep "CA:FALSE" && echo "NOT A CA CERTIFICATE" && exit 1
+openssl verify -verbose -show_chain -CAfile ${ROOT_CA_CERT} ${INTERMEDIATE_CA_CERT}
 
 ##############
 # Create local
 # device RSA 2048
 ##############
-cd ${LOCAL_CA_DIR}
+cd ${INTERMEDIATE_CA_DIR}
 # Generate private key
 openssl genrsa \
     -out ${RSA_DEV_KEY} \
@@ -124,8 +183,8 @@ openssl ca \
   -config ${RSA_SSL_CONFIG} \
   -days 375 \
   -in ${RSA_DEV_CSR} \
-  -cert ${LOCAL_CA_CERT} \
-  -keyfile ${LOCAL_CA_KEY} \
+  -cert ${INTERMEDIATE_CA_CERT} \
+  -keyfile ${INTERMEDIATE_CA_KEY} \
   -outdir /tmp/ \
   -extensions usr_cert \
   -extfile ${RSA_SSL_CONFIG} \
@@ -137,11 +196,16 @@ openssl x509 \
     -out ${RSA_DEV_CERT_DER} \
     -outform DER
 
+echo "Check entity cert chain to root cert"
+cat ${RSA_DEV_CERT}
+openssl x509 -noout -text -in ${RSA_DEV_CERT}
+openssl verify -verbose -show_chain -CAfile ${ROOT_CA_CERT} -untrusted ${INTERMEDIATE_CA_CERT} ${RSA_DEV_CERT}
+
 ##############
 # Create local
 # device ECC p256
 ##############
-cd ${LOCAL_CA_DIR}
+cd ${INTERMEDIATE_CA_DIR}
 # Generate private key
 openssl ecparam -genkey \
     -name prime256v1 \
@@ -167,8 +231,8 @@ openssl ca \
   -config ${ECC_SSL_CONFIG} \
   -days 375 \
   -in ${ECC_DEV_CSR} \
-  -cert ${LOCAL_CA_CERT} \
-  -keyfile ${LOCAL_CA_KEY} \
+  -cert ${INTERMEDIATE_CA_CERT} \
+  -keyfile ${INTERMEDIATE_CA_KEY} \
   -outdir /tmp/ \
   -extensions usr_cert \
   -extfile ${ECC_SSL_CONFIG} \
@@ -179,14 +243,21 @@ openssl x509 \
     -in ${ECC_DEV_CERT} \
     -out ${ECC_DEV_CERT_DER} \
     -outform DER
-# cat ${RSA_SSL_CONFIG}
+
+echo "Check entity cert chain to root cert"
+cat ${ECC_DEV_CERT}
+openssl x509 -noout -text -in ${ECC_DEV_CERT}
+openssl verify -verbose -show_chain -CAfile ${ROOT_CA_CERT} -untrusted ${INTERMEDIATE_CA_CERT} ${ECC_DEV_CERT}
+
 ##############
 # Copy files
 # for export
 ##############
 cp -t ${CRYPTO_EXPORT_PATH} \
-    ${LOCAL_CA_KEY} \
-    ${LOCAL_CA_CERT} \
+    ${ROOT_CA_KEY} \
+    ${ROOT_CA_CERT} \
+    ${INTERMEDIATE_CA_KEY} \
+    ${INTERMEDIATE_CA_CERT} \
     ${RSA_DEV_KEY} \
     ${RSA_DEV_CERT_DER} \
     ${RSA_DEV_CSR} \
@@ -196,6 +267,12 @@ cp -t ${CRYPTO_EXPORT_PATH} \
     ${ECC_DEV_CSR} \
     ${ECC_DEV_CERT}
 
+# cat ${INTERMEDIATE_CA_CERT} ${ROOT_CA_CERT} > ${CRYPTO_EXPORT_PATH}/chain.pem
+# cat ${CRYPTO_EXPORT_PATH}/chain.pem
+# cat ${RSA_DEV_CERT}
+
+
+
 ##############
 # Print results
 ##############
@@ -204,8 +281,10 @@ ls -lah ${CRYPTO_EXPORT_PATH}
 echo "Crypto builder phase complete!"
 
 echo "####### CERTIFICATE METADATA #######"
-echo "## CA certificate:"
-openssl x509 -text -noout -in ${LOCAL_CA_CERT}
+echo "## Root CA certificate:"
+openssl x509 -text -noout -in ${ROOT_CA_CERT}
+echo "## Intermediate CA certificate:"
+openssl x509 -text -noout -in ${INTERMEDIATE_CA_CERT}
 echo "## Entity certificates:"
 echo "#################### LOCAL RSA DEV CERTIFICATE ####################"
 openssl x509 -noout -text -in ${RSA_DEV_CERT}
